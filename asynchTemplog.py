9382894f27bb1cf338e986f4c1952e2cd37d4db7
cub525 <49500036+cub525@users.noreply.github.com>
2022-04-07 23:55:05 -0600

    feat: Pandas implementation

--- asynchTemplog.py
+++ asynchTemplog.py
@@ -7,71 +7,46 @@ Spyder Editor
 from w1thermsensor import W1ThermSensor
 
 import time
+import pandas as pd
+from datetime import datetime
+import numpy as np
 
-#Set up Sensor
-
-
-    
-#sensor = W1ThermSensor()
-
-#Setup log file
-#logging.basicConfig(filename = time.strftime("%Y%m%d%H%M%S",time.localtime()) + '_tempLogInF.csv',
-#                    format='%(message)s',
-#                    filemode='w')
-#
-#first = True
 LSB = 0.115 #deg F
-#first = 
-#name_list = [];
-#name_list.append("YYYYMMDDHHMMSS")
-header = 'time (YYYYMMDDHHMMSS), Temp (deg F)\n'
-#this version will not work if sensors are added or removed during measurement.
-try:
-    #initialize the lists    
-    sensors = []
-    current_temp = []
-    last_temp = []
-    first = []
-    log = []
+
+try:  
+    temperature_log = pd.DataFrame({
+        "Sensor" + sensor.id: []
+        for sensor in W1ThermSensor.get_available_sensors()
+    })
+
     #append to each of the lists
-    for sensor in W1ThermSensor.get_available_sensors():   
-        sensors.append(sensor.id) 
-        first.append(True)
-        current_temp.append(sensor.get_temperature(W1ThermSensor.DEGREES_F))
-        last_temp.append(sensor.get_temperature(W1ThermSensor.DEGREES_F))
-        log.append(open(time.strftime("%Y%m%d%H%M%S",time.localtime()) + '_' + sensor.id + '_tempLogInF.csv','w'))
-    #
+    for sensor in W1ThermSensor.get_available_sensors():
+        row = pd.DataFrame(
+                {"Sensor" + sensor.id: 
+                sensor.get_temperature(W1ThermSensor.DEGREES_F) 
+                }, index=[datetime.now()])
+        pd.concat([temperature_log, row])
+
     while (True):
-        for sensor in W1ThermSensor.get_available_sensors():
-            index = sensors.index(sensor.id)
-            current_temp[index] = sensor.get_temperature(W1ThermSensor.DEGREES_F)
-            screen_output = sensor.id + ',' + str(current_temp[index])
-            file_output = time.strftime("%Y%m%d%H%M%S",time.localtime()) + ',' + str(current_temp[index]) + '\n'
-           
-            if first[index]:
-                #output the header        
-                log[index].write(header)
-                #initialize the list for time and sensor output 
-                last_temp[index] = current_temp[index]
-                #output temp_list        
-                print(screen_output)
-                log[index].write(file_output)
-                first[index] = False
-            elif abs(current_temp[index]- last_temp[index]) > LSB:
-                #record
-                last_temp[index] = current_temp[index]
-                print(screen_output)
-                log[index].write(file_output)
+        row = pd.DataFrame(
+                {"Sensor" + sensor.id: 
+                (value := sensor.get_temperature(W1ThermSensor.DEGREES_F))
+                for sensor in W1ThermSensor.get_available_sensors()
+                if abs(value - temperature_log["Sensor" + sensor.id]) > LSB
+                }, index=[datetime.now()])
+        pd.concat([temperature_log, row])
+
         
         time.sleep(6)
 
 except KeyboardInterrupt:
-    for sensor in W1ThermSensor.get_available_sensors():
-        index = sensors.index(sensor.id)
-        current_temp[index] = sensor.get_temperature(W1ThermSensor.DEGREES_F)
-        file_output = time.strftime("%Y%m%d%H%M%S",time.localtime()) + ',' + str(current_temp[index]) + '\n'
-        log[index].write(file_output)
-finally: # Ensure that the file is always closed.
-    log[index].close()
-    print "Done Logging Temp"
+    row = pd.DataFrame(
+                {f"Sensor{sensor.id}": 
+                sensor.get_temperature(W1ThermSensor.DEGREES_F) 
+                }, index=[datetime.now()])
+    pd.concat([temperature_log, row])
+finally:
+    for column in temperature_log:
+        temperature_log[column].dropna().tocsv(df.index[0].strftime('%Y%m%d%H%M%S') + '_' + sensor.id + '_tempLogInF.csv')
+    # print "Done Logging Temp"
     
